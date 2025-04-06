a564854b4806ec6a5c65e78db9eeb6eb2c2c67e5
Diego <149812280+Diegooaraujo@users.noreply.github.com>
2025-04-06 16:06:46 -0300

    iniciando orientacao a objetos

---
+++ orientacaoObjetos/src/Filme.java
@@ -0,0 +1,8 @@
+public class Filme {
+    String nome ;
+    int anoDeLancameto;
+    boolean includoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+}
---
+++ orientacaoObjetos/src/Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome ="o poderoso chefão";
+        meuFilme.anoDeLancameto = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        System.out.println(meuFilme.nome);
+        System.out.println(meuFilme.anoDeLancameto);
+
+
+    }
+}
