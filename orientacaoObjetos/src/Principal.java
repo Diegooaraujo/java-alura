582f3ed20a48ca6f871d21a99af604c626ea3f3a
Diego <diegoubatuba103@gmail.com>
2025-04-17 12:25:35 -0300

    get and seters

--- orientacaoObjetos/src/Principal.java
+++ orientacaoObjetos/src/Principal.java
@@ -1,12 +1,19 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome ="o poderoso chefão";
-        meuFilme.anoDeLancameto = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("o poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.avalia(8);
+        meuFilme.avalia(6);
+        meuFilme.avalia(10);
 
-        System.out.println(meuFilme.nome);
-        System.out.println(meuFilme.anoDeLancameto);
+        meuFilme.exibeFichaTecnica();
+        System.out.println("total de avaliações"+ meuFilme.getTotalDeAvaliacoes());
+        System.out.println("Duração em minutos"+ meuFilme.getDuracaoEmMinutos());
+
 
 
     }
